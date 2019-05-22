6f37e92577d1567fc6269426434e23c62bb4ca45
carlin connell <carlin.connell@gmail.com>
2019-05-22 19:41:40 +1000

    Added the converted python file!

---
+++ main/main.py
@@ -0,0 +1,33 @@
+'''
+https://learn.adafruit.com/adafruit-mma8451-accelerometer-breakout/python-circuitpython
+https://github.com/adafruit/Adafruit_Python_BME280
+https://learn.adafruit.com/adafruit-triple-axis-gyro-breakout/python-circuitpython
+https://raspberrypi.stackexchange.com/questions/14153/adafruit-i2c-library-problem
+'''
+
+import time
+import board
+import busio
+import adafruit_l3gd20
+import adafruit_mma8451
+import adafruit_bme280
+ 
+# Hardware I2C setup:
+I2C = busio.I2C(board.SCL, board.SDA)
+SENSOR_GYRO = adafruit_l3gd20.L3GD20_I2C(I2C)
+SENSOR_ACCELEROMETER = adafruit_mma8451.MMA8451(I2C) 
+SENSOR_TEMPERATURE = adafruit_bme280.Adafruit_BME280_I2C(I2C)
+SENSOR_TEMPERATURE.sea_level_pressure = 1013.25
+
+while True:
+	print("\nTemperature: %0.1f C" % SENSOR_TEMPERATURE.temperature)
+	print("Humidity: %0.1f %%" % SENSOR_TEMPERATURE.humidity)
+	print("Pressure: %0.1f hPa" % SENSOR_TEMPERATURE.pressure)
+	print("Altitude = %0.2f meters" % SENSOR_TEMPERATURE.altitude)	
+	print('Angular Momentum (rad/s): {}'.format(SENSOR_GYRO.gyro))
+	x, y, z = SENSOR_ACCELEROMETER.acceleration
+	print('Acceleration: x={0:0.3f}m/s^2 y={1:0.3f}m/s^2 z={2:0.3f}m/s^2'.format(x, y, z))
+	time.sleep(1)
+
+
+ 
